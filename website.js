011a8e8c3a6a1efa25433775bc4771a38a0c3463
Prateek Raghav <140311025+pratiikraghav@users.noreply.github.com>
2023-07-30 02:56:21 +0530

    Add files via upload

---
+++ website.js
@@ -0,0 +1,20 @@
+let btn = document.getElementById("btn")
+let c = document.getElementById("circle")
+let d = document.getElementById("light")
+let e = document.getElementById("edd")
+let z = document.getElementById("menu")
+let x = document.getElementById("edited")
+let k = document.getElementById("contain")
+function funck(){
+    btn.classList.toggle("act");
+ 
+    c.classList.toggle("act2");
+    d.classList.toggle("op");
+    e.classList.toggle("edddd");  
+}
+
+function hide(){
+    
+    k.classList.toggle("java")
+    x.classList.toggle("script")
+}
